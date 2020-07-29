d04cf88d54d4c4806e9b83a2697ad788ea71bdd1
sambell <bell137@gmail.com>
2020-07-29 17:02:24 -0500

    renamed packages

--- bookapp/books/routes/book.py
+++ bookapp/books/routes/book.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request, Response
-from bookapp.books.schemas.book import BookSchema
-from bookapp.books.models.book import BookModel
+from bookapp.books.schema.book import BookSchema
+from bookapp.books.model.book import BookModel
 from bookapp import db
 from marshmallow.exceptions import ValidationError
 
--- bookapp/books/schemas/book.py
+++
@@ -1,6 +0,0 @@
-from bookapp.books.models.book import BookModel
-from marshmallow_sqlalchemy import ModelSchema
-
-class BookSchema(ModelSchema):
-    class Meta:
-        model = BookModel
